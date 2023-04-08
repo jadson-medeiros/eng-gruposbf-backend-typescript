849f1ef541c12c9d8aff981ddf1220047b6157c8
jadson-medeiros <developer.medeiros@gmail.com>
2023-04-07 23:22:03 -0300

    updated server.ts log

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import { env } from './config/env';
 
 app.listen({
     host: '0.0.0.0',
-    port: env.PORT
-}).then(() => {
-    console.log('HTTP server running! 🚀');
+    port: env.PORT ? Number(env.PORT) : 3333,
+}, function (err, address) {
+    if (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
+    app.log.info(`server listening on ${address}`);
 });
